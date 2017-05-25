4aafe0cf00b1c9237b1d2b029205ad55cf5a3839
Dylan <dylan.patrick.cheng@gmail.com>
2017-05-25 11:25:10 -0700

    expand on detail template. change Http404 and try/except to get_object_or_404

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
-from django.http import Http404, HttpResponse
+from django.http import HttpResponse
 
 from .models import Question
 
@@ -18,10 +18,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
