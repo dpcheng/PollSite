37df55fb11ef200ef2274465c46ae11cff31f2d8
Dylan <dylan.patrick.cheng@gmail.com>
2017-06-26 21:50:41 -0700

    create style.css in polls/static/polls

--- polls/tests.py
+++ polls/tests.py
@@ -9,6 +9,7 @@ from django.test import TestCase
 from .models import Question
 # Create your tests here.
 
+
 class QuestionMethodTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
