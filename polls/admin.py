0e50e665bc694eed8a3e10dd9386e35cbd4ac6c0
Dylan <dylan.patrick.cheng@gmail.com>
2017-06-26 22:32:33 -0700

    add TabularInline choices for admin question form

--- polls/admin.py
+++ polls/admin.py
@@ -7,11 +7,17 @@ from .models import Question, Choice
 # Register your models here.
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,                  {'fields': ['question_text']}),
-        ('Date information',    {'fields': ['pub_date']})
+        ('Date information',    {'fields': ['pub_date'], 'classes': ['collapse']})
     ]
+    inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
